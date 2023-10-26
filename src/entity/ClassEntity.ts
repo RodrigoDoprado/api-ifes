e1a22c381d17c0b920e8113b4141c376f1c60e72
Rodrigo do Prado <rodrigodoprado1992@gmail.com>
2023-10-26 07:10:18 -0300

    fix: all project

--- src/entity/ClassEntity.ts
+++ src/entity/ClassEntity.ts
@@ -1,40 +1,39 @@
-// /* eslint-disable @typescript-eslint/no-unused-vars */
-// import {
-//   Entity,
-//   Column,
-//   CreateDateColumn,
-//   UpdateDateColumn,
-//   PrimaryGeneratedColumn,
-//   DeleteDateColumn,
-//   JoinColumn,
-//   ManyToOne,
-// } from "typeorm"
-// import { CourseEntity } from "./CourseEntity"
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+  DeleteDateColumn,
+  JoinColumn,
+  ManyToOne,
+} from "typeorm"
 
-// @Entity("classes") //turmas
-// export class ClassEntity {
-//   @PrimaryGeneratedColumn()
-//   public id: string
+@Entity("classes") //turmas
+export class ClassEntity {
+  @PrimaryGeneratedColumn()
+  public id: string
 
-//   @Column({ type: "text", nullable: false })
-//   public title: string
+  @Column({ type: "text", nullable: false })
+  public title: string
 
-//   @Column({ type: "text", nullable: false })
-//   public acronym: string //sigla
+  @Column({ type: "text", nullable: false })
+  public acronym: string //sigla
 
-//   @Column({ type: "text", nullable: false })
-//   public Shift: string //turno
+  @Column({ type: "text", nullable: false })
+  public Shift: string //turno
 
-//   @ManyToOne((type) => CourseEntity, (classes) => classes, { nullable: false })
-//   @JoinColumn({ name: "course_id", referencedColumnName: "id" })
-//   public course: CourseEntity
+  //   @ManyToOne((type) => CourseEntity, (classes) => classes, { nullable: false })
+  //   @JoinColumn({ name: "course_id", referencedColumnName: "id" })
+  //   public course: CourseEntity
 
-//   @CreateDateColumn()
-//   public created_at: Date // Creation date
+  @CreateDateColumn()
+  public created_at: Date // Creation date
 
-//   @UpdateDateColumn()
-//   public updated_at: Date // Last updated date
+  @UpdateDateColumn()
+  public updated_at: Date // Last updated date
 
-//   @DeleteDateColumn()
-//   public deleted_at: Date // Deletion date
-// }
+  @DeleteDateColumn()
+  public deleted_at: Date // Deletion date
+}
